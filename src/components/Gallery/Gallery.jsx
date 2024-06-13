5c97d2b8ced169197f5d9b51e66ab45ef971025c
Gnarkill33 <gnarkill33@gmail.com>
2024-06-13 20:19:18 +0300

    filters_page

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,40 @@
+import { mock } from '../../data.mock.js';
+import Navbar from '../NavBar/NavBar';
+import Filters from '../Filters'
+import Card from '../Card/Card';
+import SearchButton from '../SearchButton/SearchButton';
+import { JoinButton } from '../JoinWindow/JoinWindow.style.js';
+import { FilterWrapper } from '../MainSection/MainSection.style.js';
+import { Container, Results, CardWrapper } from './Gallery.style.js';
+import { JoinWindowTitle } from '../JoinWindow/JoinWindow.style.js';
+
+
+const Gallery = () => {
+    return (
+        <Container>
+            <Navbar />
+            <JoinWindowTitle $textColor='var(--white)' $margin='5rem 0 3rem 0'>
+                All Villas
+                <Results>124 results</Results>
+            </JoinWindowTitle>    
+            <FilterWrapper $marginTop='3rem' $marginBottom='2rem'>
+                <Filters />
+                <SearchButton />
+            </FilterWrapper>
+            <JoinButton 
+            $small $gold='var(--gold-gold-light)' 
+            $transparent='transparent' 
+            $margin='0 0 0 14rem'>
+                ADD FILTERS
+            </JoinButton>
+            <CardWrapper>
+                {mock.map((item) => {
+                return <Card key={item.id} {...item} />
+                })
+                }
+            </CardWrapper>
+        </Container>
+    )
+}
+
+export default Gallery;
---
+++ src/components/Gallery/Gallery.style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import Image from '/images/filters.svg';
+
+export const Container = styled.div`
+    background-image: url(${Image});
+    background-size: cover;
+    background-repeat: no-repeat;
+`;
+
+export const Results = styled.div`
+    color: var(--white);
+    font-family: Raleway;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+    letter-spacing: 0%;
+    text-align: center;
+`;
+
+export const CardWrapper = styled.div`
+    display: grid;
+    justify-content: space-between;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-gap: 25px;
+    margin: 2rem 14rem;
+
+`;
